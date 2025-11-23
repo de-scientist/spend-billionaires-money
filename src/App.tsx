4922db5ce6b34b21e6442b30ccbc8c4d7407b14f
de-scientist <gitaumark502@gmail.com>
2025-11-23 11:59:51 +0300

    update the App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import ItemCard from "./components/ItemCard";
+import { useStore } from "./store/useStore";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+const { items, balance } = useStore();
+
+
+return (
+<div>
+<Navbar />
+
+
+<div className="text-center text-xl font-bold bg-green-700 text-white py-3">
+Current balance: ${balance.toLocaleString()}
+</div>
+
+
+<div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+{items.map((i) => (
+<ItemCard key={i.id} {...i} />
+))}
+</div>
+</div>
+);
 }
-
-export default App
